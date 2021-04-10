6025bcbe265983e412e940a88a5fd1ee9b593965
PatTheAtak <patrickatak15@gmail.com>
2021-04-10 15:16:54 +0300

    First Commit

---
+++ LeapYear.py
@@ -0,0 +1,16 @@
+# Check the README to understand how leap years work.
+#
+#
+
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap")
+        else:
+            print("Not Leap")
+    else:
+        print("Leap")
+else:
+    print("Not Leap")
